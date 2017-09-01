ab864d5e02c7b67c7483bcab7df41b8caa04e1bf
halo_RY <halo_RY@example.com>
2017-09-01 11:52:57 +0800

    e3-manager

--- e3-content/e3-content-interface/src/main/java/cn/e3/content/service/ContentService.java
+++ e3-content/e3-content-interface/src/main/java/cn/e3/content/service/ContentService.java
@@ -1,6 +1,9 @@
 package cn.e3.content.service;
 
+import java.util.List;
+
 import cn.e3.pojo.TbContent;
+import cn.e3.utils.AdItem;
 import cn.e3.utils.DatagridPageBean;
 import cn.e3.utils.E3mallResult;
 
@@ -19,4 +22,13 @@ public interface ContentService {
 	 * 返回值:E3mallResult
 	 */
 	public E3mallResult saveContent(TbContent content);
+	
+	/**
+	 * 需求:根据分类id查询分类内容
+	 * 参数:Long categoryId
+	 * 返回值:List<AdItem>
+	 * 业务:
+	 * 	加载大广告轮播图数据
+	 */
+	public List<AdItem> findContentAdItemWithCategoryId(Long categoryId);
 }
