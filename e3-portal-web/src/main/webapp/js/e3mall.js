99b731d5a470709fd5a5100d60e4f686333ed263
halo_RY <halo_RY@example.com>
2017-09-08 11:52:03 +0800

    e3-portal-web

--- e3-portal-web/src/main/webapp/js/e3mall.js
+++ e3-portal-web/src/main/webapp/js/e3mall.js
@@ -1,17 +1,19 @@
 var TT = TAOTAO = {
 	checkLogin : function(){
-		var _ticket = $.cookie("TT_TOKEN");
+		//获取cookie中存储用户身份唯一标识token
+		var _ticket = $.cookie("E3_TOKEN");
 		if(!_ticket){
 			return ;
 		}
+		//发送ajax请求,第六步,发送ajax请求,把token传递给单点登录系统
 		$.ajax({
-			url : "http://localhost:8084/user/token/" + _ticket,
+			url : "http://localhost:8088/user/token/" + _ticket,
 			dataType : "jsonp",
 			type : "GET",
 			success : function(data){
 				if(data.status == 200){
 					var username = data.data.username;
-					var html = username + "，欢迎来到淘淘！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
+					var html = username + "，欢迎来到宜立方！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
 					$("#loginbar").html(html);
 				}
 			}
